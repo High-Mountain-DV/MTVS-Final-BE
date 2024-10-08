ae8483cdd71e70441a3d924110eb0b357cbbe8a2
dev-hjk <gpwls7357@naver.com>
2024-10-08 16:56:14 +0900

    first commit

---
+++ src/main/java/com/khj/mtvsfinalbe/MtvsFinalBeApplication.java
@@ -0,0 +1,13 @@
+package com.khj.mtvsfinalbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtvsFinalBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtvsFinalBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khj/mtvsfinalbe/MtvsFinalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khj.mtvsfinalbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtvsFinalBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
