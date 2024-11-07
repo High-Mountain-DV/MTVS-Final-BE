32dcfb98e0c16ebcd2b41fb5893ead54821fde99
dev-hjk <gpwls7357@naver.com>
2024-11-07 17:06:02 +0900

    Feat: CombatRequestDTO, CombatResponseDTO 생성

---
+++ src/main/java/com/khj/mtvsfinalbe/combat/domain/dto/CombatRequestDTO.java
@@ -0,0 +1,22 @@
+package com.khj.mtvsfinalbe.combat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CombatRequestDTO {
+    private LocalDateTime created_at;
+    private double damage_dealt;
+    private int assists;
+    private int play_time;
+    private int score;
+    private double accuracy;
+    private String nickname;
+    private double awareness;
+    private int ally_injuries;
+    private int ally_deaths;
+    private int kills;
+    private Long memberId;
+}
---
+++ src/main/java/com/khj/mtvsfinalbe/combat/domain/dto/CombatResponseDTO.java
@@ -0,0 +1,23 @@
+package com.khj.mtvsfinalbe.combat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CombatResponseDTO {
+    private Long id;
+    private LocalDateTime created_at;
+    private double damage_dealt;
+    private int assists;
+    private int play_time;
+    private int score;
+    private double accuracy;
+    private LocalDateTime last_updated;
+    private String nickname;
+    private double awareness;
+    private int ally_injuries;
+    private int ally_deaths;
+    private int kills;
+}
