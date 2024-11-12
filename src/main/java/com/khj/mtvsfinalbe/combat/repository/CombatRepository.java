c2503644cea00e6eac8b4d586707f904fd65ef94
dev-hjk <gpwls7357@naver.com>
2024-11-12 17:11:42 +0900

    feat: add Combat repository

--- src/main/java/com/khj/mtvsfinalbe/combat/repository/CombatRepository.java
+++ src/main/java/com/khj/mtvsfinalbe/combat/repository/CombatRepository.java
@@ -10,10 +10,6 @@ import java.util.List;
 
 @Repository
 public interface CombatRepository extends JpaRepository<Combat, Long> {
-
-    // user로 최신 전투 데이터를 가져오는 메서드
     Combat findTopByUserOrderByCreatedAtDesc(User user);
-
-    // user로 모든 전투 데이터를 최신순으로 가져오는 메서드
     List<Combat> findByUser(User user, Sort sort);
 }
