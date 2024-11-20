1b4a5dd568d92c7082537e1b6e4dcff8396dedbf
dev-hjk <gpwls7357@naver.com>
2024-11-20 16:39:36 +0900

    Feat: Combat 데이터 조회 로직 개선

--- src/main/java/com/khj/mtvsfinalbe/combat/repository/CombatRepository.java
+++ src/main/java/com/khj/mtvsfinalbe/combat/repository/CombatRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface CombatRepository extends JpaRepository<Combat, Long> {
     Combat findTopByUserOrderByCreatedAtDesc(User user);
     List<Combat> findByUser(User user, Sort sort);
+
+    // 이전 Combat 데이터를 조회하기 위한 메서드 추가
+    Combat findFirstByUserAndCreatedAtBeforeOrderByCreatedAtDesc(User user, LocalDateTime createdAt);
 }
